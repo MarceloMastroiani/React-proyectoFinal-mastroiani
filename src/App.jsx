4c89573d8efdd3f8f6f8af89bcfa655962e898bb
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2023-11-21 21:18:10 -0300

    cambios sugeridos por la segunda preentrega

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
+import MainRoutes from "./routes/MainRouter";
+
 import "bootstrap/dist/css/bootstrap.min.css";
-import NavBarComponent from "./components/NavBarComponent/NavBarComponent";
-import "./index.css";
 import "./components/NavBarComponent/NavBar.css"
-import ItemListContainer from "./components/ItemListContainer/ItemListContainer";
+import "./index.css";
 function App() {
+ 
+
   return (
     <div>
-      <NavBarComponent />
-      <ItemListContainer greeting="Bienvenidos a mi pagina" />
+      <MainRoutes />
     </div>
   );
 }
--- src/components/NavBarComponent/NavBarComponent.jsx
+++ src/components/NavBarComponent/NavBarComponent.jsx
@@ -3,22 +3,43 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import CartWidgetComponent from '../CartWidgetComponent/CartWidgetComponent'
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faShirt} from '@fortawesome/free-solid-svg-icons';
-const NavBarComponent = () => {
 
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+const NavBarComponent = () => {
+  const [categories, setCategories] = useState([]);
+  
+  useEffect(() => {
+    axios
+    .get('https://dummyjson.com/products/categories')
+    .then(res => setCategories(res.data))
+    .catch((error) => console.log(error));
+  }, []);
+  
   return (
+    
     <Navbar expand="lg" className="nav">
       <Container>
-        <Navbar.Brand href="#inicio" className='brand'> Tienda Mastroiani</Navbar.Brand>
+        <Navbar.Brand><Link to={"/"} className='brand'>Tienda Mastroiani</Link></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#inicio">Inicio</Nav.Link>
-            <Nav.Link href="#product">Productos</Nav.Link>
-            <NavDropdown title="Categoria" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#remeras">Remeras</NavDropdown.Item>
-              <NavDropdown.Item href="#accesorios">Accesorios</NavDropdown.Item>
+            <NavDropdown title="Categorias" id="basic-nav-dropdown">
+              {categories.map((category, index) => {
+                  return(
+                    <NavDropdown.Item Key={index}>
+                    <Link
+                   to={`/category/${category}`}
+                   style={{textDecoration: "none", color: "#360b41"}}
+                   >
+                    {category}
+                   </Link>
+                   </NavDropdown.Item>
+                  )
+                })
+              }
             </NavDropdown>
           </Nav>
           <CartWidgetComponent />
